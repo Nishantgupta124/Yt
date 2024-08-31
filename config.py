ddca2d5f39c70f2301785595f6ee36ec5793601a
Nishantgupta124 <157084471+Nishantgupta124@users.noreply.github.com>
2024-08-31 17:22:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7255048090:AAFpMpH9KDNrr3BpnlAlraE5c2UkqhlSY5w")
+BOT_TOKEN=("6510046866:AAGfbya5GGt0AVSgqGFYiVpyUUwVuHY7QXw")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("")
+OWNER_USERNAME=("Rishabgupta01")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("")
+ADMIN_IDS=("907345225")
 
 
 
